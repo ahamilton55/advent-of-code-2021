a78e83cbe059876337664a5486561cf79a28dea3
Andrew Hamilton <ahamilton55@gmail.com>
2021-12-23 07:27:34 -0800

    Adds day2 and part of day3

---
+++ day2/part2/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    //if let Ok(lines) = read_lines("./sample.txt") {
+    if let Ok(lines) = read_lines("./input.txt") {
+        let mut pos = 0;
+        let mut depth = 0;
+        let mut aim = 0;
+
+        for line in lines {
+            if let Ok(l) = line {
+                let v: Vec<&str> = l.split(' ').collect();
+                let d = v[1].parse::<i32>().unwrap();
+
+                if v[0] == "forward" {
+                    pos += d;
+                    depth += aim * d;
+                } else if v[0] == "up" {
+                    aim -= d;
+                } else if v[0] == "down" {
+                    aim += d;
+                }
+                println!("{}, {}, {}", depth, pos, aim);
+            }
+        }
+        println!("depth: {}", depth);
+        println!("pos: {}", pos);
+        println!("depth * pos: {}", depth * pos);
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
---
+++ day3/part2/src/main.rs
@@ -0,0 +1,65 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    if let Ok(lines) = read_lines("./sample.txt") {
+    //if let Ok(lines) = read_lines("./input.txt") {
+        let mut xs: [i32; 12]= [0; 12];
+        let mut count = 0;
+
+        let mut ls: Vec<_> = lines.collect();
+
+        //for line in lines {
+        //    if let Ok(l) = line {
+        //        println!("{}", l);
+        //        count += 1;
+        //        let mut i = 0;
+        //        for digit in  l.chars() {
+        //            if digit == '1' {
+        //                xs[i] += 1;
+        //            }
+        //            i += 1;
+        //        }
+        //        
+        //    }
+        //}
+
+        let mut gamma = 0;
+        let mut epsilon = 0;
+        //let mut i:u32 = (xs.len() as u32) - 1;
+        //let base:i32 = 2;
+        //println!("count: {}", count);
+        //for n in xs {
+        //    println!("n[{}]: {}", i, n);
+        //    if n >= count/2 {
+        //        println!("gamma");
+        //        gamma += base.pow(i);
+        //    } else {
+        //        println!("epsilon");
+        //        epsilon += base.pow(i);
+        //    }
+        //    if i > 0 {
+        //        i -= 1;
+        //    }
+        //}
+
+        println!("{}", gamma);
+        println!("{}", epsilon);
+        println!("{}", gamma*epsilon);
+    }
+}
+
+fn find_entries(lines: Vec<String>, gamma: bool) -> () {
+    for line in lines {
+        if let Ok(l) = line {
+
+        }
+    }
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
